ca91b792a578583cde06c91cfd4ce088aec0c997
Sammy <sammylam_1@hotmail.com>
2023-04-25 15:26:06 -0400

    working on proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "http:localhost:5000/api/data",
+			},
+		];
+	},
+};
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, useState } from "react";
+import { Data } from "./models/data";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App() {
+	//grabbing data
+	const [data, setData] = useState<Data[]>([]);
+	useEffect(() => {
+		async function loadData() {
+			try {
+				const response = await fetch("/api/data/", {
+					method: "GET",
+				});
+				const alldata = await response.json();
+				console.log(response);
+			} catch (error) {
+				console.error(error);
+				alert(error);
+			}
+		}
+		loadData();
+	}, []);
+	return <div className='App'>{JSON.stringify(data)}</div>;
 }
